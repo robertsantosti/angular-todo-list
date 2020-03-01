de405468b2a28eb7dad02f56b44061b7ba754c91
Robert Santos <robertsantosti@outlook.com>
2020-03-01 11:32:23 -0300

    Estilizando o corpo do html

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -64,6 +64,10 @@ export class AppComponent {
 
   private load() {
     const data = localStorage.getItem("todos");
-    this.todos = JSON.parse(data);
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
   }
 }
