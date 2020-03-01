86050315df9eb79bc77aff460b355255e34473d6
Robert Santos <robertsantosti@outlook.com>
2020-03-01 10:53:13 -0300

    Trabalhando com reactive forms

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { Todo } from "src/models/todo.model";
 
 @Component({
@@ -9,8 +10,19 @@ import { Todo } from "src/models/todo.model";
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = "Minhas Tarefas";
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [
+        "",
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required
+        ])
+      ]
+    });
     this.todos.push(new Todo(1, "Passear com o cachorro", false));
     this.todos.push(new Todo(2, "Ir ao supermercado", false));
     this.todos.push(new Todo(3, "Cortar o cabelo", false));
